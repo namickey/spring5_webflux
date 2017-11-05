b912c3819d8ce48244b02c8d7fb49b80e13f06ad
namickey <teethdai@gmail.com>
2017-11-05 20:35:44 +0900

    implements sample

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +17,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+
+
 }
--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @RestController
 public class Hello {
+
     @GetMapping("/")
-    Flux<String> hello(){
-       return Flux.just("hello world.");
+    Flux<String> hello() {
+        return Flux.just("hello world.");
+    }
+
+    @GetMapping("/a")
+    Flux<String> a() {
+        return Flux.just("/a hello.");
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routes() {
+        return route(GET("/b"), req -> ok().syncBody("/b webFlux."));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> index() {
+        return route(GET("/c"), req -> ok().syncBody("/c syncbody."));
     }
 }
